3c49e2bea19a0ddec44bec086e40e5e196e5c4b9
Jacek Sawko <jacek.sawko@ttpsc.pl>
2019-12-18 12:16:00 +0100

    start of project

---
+++ src/main/java/tt/psc/testDB/Main.java
@@ -0,0 +1,31 @@
+package tt.psc.testDB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String user = "springstudent";
+        String pass = "springstudent";
+        String jdbcUrl = "jdbc:mysql://localhost:3306/web_customer_tracker?useSSL=false&serverTimezone=UTC";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try(Connection connection = DriverManager.getConnection(jdbcUrl, user, pass)) {
+            if (connection.isValid(100)){
+                System.out.println(connection.getCatalog());
+                System.out.println("XDDDD");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ src/main/webapp/WEB-INF/view/list-customers.jsp
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <title>List of Customers</title>
+</head>
+
+<body>
+    List Customers - coming soon ...
+</body>
+
+</html>
