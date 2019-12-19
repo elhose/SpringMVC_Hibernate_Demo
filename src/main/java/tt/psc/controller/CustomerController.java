15b7091181ab748ebef3bdb23b03ecfae193b17e
Jacek Sawko <jacek.sawko@ttpsc.pl>
2019-12-19 14:29:48 +0100

    działa, przed dodaniem css

--- src/main/java/tt/psc/controller/CustomerController.java
+++ src/main/java/tt/psc/controller/CustomerController.java
@@ -1,15 +1,31 @@
 package tt.psc.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import tt.psc.dao.CustomerDAO;
+import tt.psc.entity.Customer;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
+    //need to inject customer DAO
+    @Autowired
+    private CustomerDAO customerDAO;
+
     @RequestMapping("/list")
-    public String listCustomers(Model model){
+    public String listCustomers(Model model) {
+
+        //get customers from the dao
+        List<Customer> customersList = customerDAO.getCustomers();
+
+        //add customers to model
+        model.addAttribute("customers", customersList);
+
         return "list-customers";
     }
 }
---
+++ src/main/java/tt/psc/dao/CustomerDAO.java
@@ -0,0 +1,9 @@
+package tt.psc.dao;
+
+import tt.psc.entity.Customer;
+
+import java.util.List;
+
+public interface CustomerDAO {
+     List<Customer> getCustomers();
+}
